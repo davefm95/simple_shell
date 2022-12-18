43cc59e96d39f4467a152b47722e9f0bd2901ef4
Femi Adetoye <davefm95@gmail.com>
2022-12-17 22:15:24 -0800

    added test files

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#endif
